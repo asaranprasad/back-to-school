bca581d01b49431b210342a3c513fb3f3a327019
asaranprasad <ambikapathy.s@husky.neu.edu>
2018-02-15 02:19:33 -0500

    practice

--- src/leetcode/TwoSum.java
+++ src/leetcode/TwoSum.java
@@ -7,6 +7,11 @@ package leetcode;
 import java.util.HashMap;
 
 public class TwoSum {
+
+  /* O(n) RunTime
+   * O(n) Extra Space
+   */
+
   public int[] twoSum(int[] nums, int target) {
     HashMap<Integer, Integer> map = new HashMap<Integer, Integer>();
     for (int i = 0; i < nums.length; i++) {
@@ -18,5 +23,28 @@ public class TwoSum {
     }
     return null;
   }
+
+
+  /* O(nlogn) RunTime
+   * Constant Extra Space
+   * Modifies the original array.
+   * Returns: the actual values
+   * O(n) extra space required if needed to remember the original index
+   */
+  //  public int[] twoSum(int[] nums, int target) {
+  //    Arrays.sort(nums);
+  //    int i = 0;
+  //    int j = nums.length - 1;
+  //    while (i < j) {
+  //      int sum = nums[i] + nums[j];
+  //      if (sum == target)
+  //        return new int[] {nums[i], nums[j]};
+  //      if (sum > target)
+  //        j--;
+  //      else
+  //        i++;
+  //    }
+  //    return null;
+  //  }
 }
 
