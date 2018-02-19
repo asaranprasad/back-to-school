f240b51e01cbe59da0a61b8e9c01edae644db581
asaranprasad <ambikapathy.s@husky.neu.edu>
2018-02-19 02:00:33 -0500

    practise

--- src/misc/Permutation.java
+++ src/misc/Permutation.java
@@ -2,31 +2,34 @@ package misc;
 
 public class Permutation {
 
-	public static void main(String[] args) {
-		String str = "abc";
-		char[] s = str.toCharArray();
-		permute(s, 0, s.length - 1);
+  public static void main(String[] args) {
+    String str = "abc";
+    char[] s = str.toCharArray();
+    //    System.out.println(s);
+    permute(s, 0, s.length - 1);
 
-	}
+  }
 
-	private static void permute(char[] a, int fixedIndex, int lastIndex) {
-		{
-			int i;
-			if (fixedIndex == lastIndex)//makes sure we do not print anything but the leaf nodes
-				System.out.println(a);
-			else {
-				for (i = fixedIndex; i <= lastIndex; i++) {
-					swap(a, fixedIndex, i);
-					permute(a, fixedIndex + 1, lastIndex);
-					swap(a, fixedIndex, i); // backtrack
-				}
-			}
-		}
-	}
 
-	private static void swap(char[] s, int index1, int index2) {
-		char temp = s[index1];
-		s[index1] = s[index2];
-		s[index2] = temp;
-	}
+
+  private static void permute(char[] s, int start, int end) {
+    if (start >= end)
+      System.out.println(s);
+
+    else
+      for (int i = start; i <= end; i++) {
+        swap(s, start, i);
+        permute(s, start + 1, end);
+        swap(s, start, i);
+      }
+
+  }
+
+
+
+  private static void swap(char[] s, int index1, int index2) {
+    char temp = s[index1];
+    s[index1] = s[index2];
+    s[index2] = temp;
+  }
 }
