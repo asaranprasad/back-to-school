f5927db2ba75aa155fe366e6d205e267b1febe62
asaranprasad <ambikapathy.s@husky.neu.edu>
2018-04-08 10:41:32 -0400

    Modified the Graph's code to add edges more intuitively

--- src/graphs/Vertex.java
+++ src/graphs/Vertex.java
@@ -6,4 +6,5 @@ public class Vertex {
   public int key;
   public Vertex p;
   public DNode d; // pointer to doubly link representation in heaps
+  public int graphIndex; // index to Adjacency list in the graph
 }
