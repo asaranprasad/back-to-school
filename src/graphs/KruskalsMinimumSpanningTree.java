670f035d72ee1cfe0cafd1ba650e21c3f69c8b62
asaranprasad <ambikapathy.s@husky.neu.edu>
2018-04-07 21:45:29 -0400

    implementing disjoint sets

--- src/graphs/KruskalsMinimumSpanningTree.java
+++ src/graphs/KruskalsMinimumSpanningTree.java
@@ -1,5 +1,18 @@
 package graphs;
 
+
+class Node{
+  public Node p;
+  public Node next;
+}
+
+class DisjointSets{
+ public Node head;
+ public Node tail;
+ 
+  
+}
+
 public class KruskalsMinimumSpanningTree {
 
   public void mstKruskal(Graph G) {
