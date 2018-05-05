0ba913ab5273d7bcbbc2dcb32bd2980d4d01c83a
asaranprasad <ambikapathy.s@husky.neu.edu>
2018-05-05 15:47:53 -0400

    base solution to substringMatcihng

--- src/google/SubStringMatching.java
+++ src/google/SubStringMatching.java
@@ -5,6 +5,10 @@ public class SubStringMatching {
   /**
    * Find the number of times string A has to be repeated so that string B becomes a substring of A
    * 
+   * Eg: A = "abcd" ; B = "cdabcdabc"
+   * 
+   * Sol: 3
+   * 
    * @param A
    * @param B
    * @return
@@ -12,52 +16,54 @@ public class SubStringMatching {
   public int solution(String A, String B) {
     int n = A.length();
     int m = B.length();
-    int i = 0;
-    int j = 0;
-    int rem = 0;
-    while (j < m) {
-      while (A.charAt(i) != B.charAt(j) && j < m) {
-        j++;
+
+    int fromIndex = findStartsWithSubStr(B, A);
+    int aPtr = 0;
+    int bPtr = fromIndex;
+    int aRep = bPtr == 0 ? 0 : 1;
+    while (bPtr < m) {
+      if (B.charAt(bPtr) != A.charAt(aPtr))
+        return -1;
+      bPtr++;
+      aPtr++;
+      if (aPtr >= n) {
+        aRep++;
+        aPtr = 0;
       }
-      rem = j;
-
-      // first half
-      boolean flag = true;
-      while (i < n && j < m) {
-        if (A.charAt(i) == B.charAt(j)) {
-          i++;
-          j++;
-          continue;
-        } else {
-          flag = false;
-          break;
-        }
-      }
-
-      if (flag == false) {
-        j = (m + rem + 1) % m;
-        continue;
-      }
-
-      // second half
-
-
-
-    }
-    if (A.charAt(i) != B.charAt(j)) {
-      i = 0;
-      j = rem + 1;
-      //      continue;
     }
 
+    // if after cycling-around B, A does not continue
+    // => new set of A is required
+    if (B.charAt(0) != A.charAt(aPtr))
+      aRep++;
 
-    return -1;
-
+    return aRep;
   }
 
-  public static void main(String[] args) {
-    // TODO Auto-generated method stub
 
+  private int findStartsWithSubStr(String bigger, String a) {
+    for (int i = 0; i < a.length(); i++) {
+      String substrA = a.substring(i, a.length());
+      if (bigger.startsWith(substrA))
+        return i;
+    }
+    return -1;
+  }
+
+
+  private int findEndsWithSubStr(String bigger, String a) {
+    for (int i = a.length(); i >= 0; i--) {
+      String substrA = a.substring(0, i);
+      if (bigger.endsWith(substrA))
+        return bigger.length() - substrA.length();
+    }
+    return -1;
+  }
+
+
+  public static void main(String[] args) {
+    SubStringMatching ssm = new SubStringMatching();
+    System.out.println(ssm.solution("abcd", "cdabcdabc")); // Expecting 3
   }
 
 }
