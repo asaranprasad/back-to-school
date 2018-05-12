a21ff0274af201f4e165c431230b3f1318fa4329
Saran Prasad Ambikapathy <ambikapathy.s@husky.neu.edu>
2018-05-11 20:06:38 -0400

    Course Schedule leetcode

---
+++ src/leetcode/CourseSchedule.java
@@ -0,0 +1,59 @@
+//https://leetcode.com/problems/course-schedule/description/
+
+package leetcode;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class CourseSchedule {
+
+  public boolean canFinish(int numCourses, int[][] prerequisites) {
+    List<Integer>[] adj = new LinkedList[numCourses];
+
+    for (int i = 0; i < numCourses; i++)
+      adj[i] = new LinkedList<Integer>();
+
+    for (int[] p : prerequisites) {
+      //adj[p[1]].add(p[0]);
+      adj[p[0]].add(p[1]);
+    }
+
+    // 0 - not visited (White)
+    // 1 - visited (Black)
+    // 2 - currently in traversal (Gray)
+    int[] visited = new int[numCourses];
+
+    for (int i = 0; i < numCourses; i++) {
+      if (visited[i] == 0) {
+        if (!dfsCheck(adj, i, visited))
+          return false;
+      }
+    }
+    return true;
+  }
+
+  // returns false if a cycle is found
+  public boolean dfsCheck(List<Integer>[] adj, int node, int[] visited) {
+    for (int neigh : adj[node]) {
+      if (visited[neigh] == 2)
+        return false; // Cycle exists, i.e. visiting a parent Gray node
+      visited[neigh] = 2; // mark as Gray
+      boolean isTree = dfsCheck(adj, neigh, visited);
+      if (!isTree)
+        return false;
+      visited[neigh] = 1;
+    }
+    return true;
+  }
+
+  public static void main(String[] args) {
+    CourseSchedule cs = new CourseSchedule();
+    System.out.println(!cs.canFinish(2, new int[][] {{1, 0}, {0, 1}}) ? "pass" : "fail");
+    System.out.println(cs.canFinish(3, new int[][] {{1, 0}, {2, 0}}) ? "pass" : "fail");
+    System.out.println(cs.canFinish(2, new int[][] {{1, 0}}) ? "pass" : "fail");
+
+  }
+
+}
+
+
