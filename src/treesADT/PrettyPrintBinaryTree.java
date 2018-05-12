18fe5d93528e973a371763f353060307adb0909f
asaranprasad <ambikapathy.s@husky.neu.edu>
2018-05-12 03:14:31 -0400

    Build Bin tree of characters

--- src/treesADT/PrettyPrintBinaryTree.java
+++ src/treesADT/PrettyPrintBinaryTree.java
@@ -1,10 +1,48 @@
 package treesADT;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class PrettyPrintBinaryTree {
 
-  public static void main(String[] args) {
-    // TODO Auto-generated method stub
+  class Node {
+    char data;
+    Node left;
+    Node right;
+
+    public Node(char data) {
+      this.data = data;
+      left = right = null;
+    }
+  }
+
+  public void prettyPrintToConsole(Node root) {
 
   }
+
+  private Node bfsFillChar256() {
+    Node root = new Node((char) 0);
+    Queue<Node> q = new LinkedList<Node>();
+    q.add(root);
+    int i = 1;
+    while (!q.isEmpty() && i < 256) {
+      Node n = q.poll();
+      Node left = new Node((char) (i++));
+      Node right = new Node((char) (i++));
+      n.left = left;
+      n.right = right;
+      q.add(left);
+      q.add(right);
+    }
+    return root;
+  }
+
+
+  public static void main(String[] args) {
+    PrettyPrintBinaryTree p = new PrettyPrintBinaryTree();
+    Node root = p.bfsFillChar256();
+  }
+
+
 
 }
