ccf9fe6f58490e146a8cf95b807a58ceaef9d85b
asaranprasad <ambikapathy.s@husky.neu.edu>
2018-06-12 23:58:56 -0400

    plusOne

--- src/leetcode/Easy.java
+++ src/leetcode/Easy.java
@@ -82,10 +82,26 @@ public class Easy {
   /* https://leetcode.com/problems/plus-one/description/ */
 
   public int[] plusOne(int[] digits) {
-    int carry = 0;
+    int carry = 1;
     for (int i = digits.length - 1; i >= 0; i--) {
-
+      if (digits[i] == 9 && carry == 1) {
+        digits[i] = 0;
+        carry = 1;
+      } else {
+        digits[i] += carry;
+        carry = 0;
+      }
     }
+
+    if (carry == 1) {
+      int[] newDigits = new int[digits.length + 1];
+      newDigits[0] = 1;
+      for (int i = 0; i < digits.length; i++) {
+        newDigits[i + 1] = digits[i];
+      }
+      return newDigits;
+    } else
+      return digits;
   }
 
   //---------------------------------------------------------------------------//
