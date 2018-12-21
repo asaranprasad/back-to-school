b42d517cb5d4d6d32c5ee0cb334be4d47d121bad
asaranprasad <ambikapathy.s@husky.neu.edu>
2018-12-21 07:55:58 +0530

    practise

--- src/leetcode/ZipList.java
+++ src/leetcode/ZipList.java
@@ -1,34 +1,65 @@
 package leetcode;
 
 
-class ListNode{
+class ListNode {
   int val;
   ListNode next;
-  ListNode(int val){ this.val = val; }
+
+  ListNode(int val) {
+    this.val = val;
+  }
 }
+
 
 public class ZipList {
   public static void main(String args[]) {
     ListNode head = new ListNode(0);
     ListNode node = head;
-    
-    for(int i = 1; i < 7; i++) {
+
+    for (int i = 1; i < 8; i++) {
       node.next = new ListNode(i);
       node = node.next;
     }
-    
+
     ZipList z = new ZipList();
     ListNode zipped = z.zipList(head);
-    
+
     z.printList(zipped);
   }
-  
+
+  // 0 1 2 3 4 5 6 7 
   private ListNode zipList(ListNode head) {
+    ListNode sp = head;
+    ListNode fp = head;
+
+    // find the middle of the lsit
+    while (fp.next != null && fp.next.next != null) {
+      System.out.println("fp: " + fp.val + " sp: " + sp.val);
+      sp = sp.next;
+      fp = fp.next.next;
+    }
+
+    ListNode toBeRev = sp.next;
+    sp.next = null;
+
+    // Reverse the second half of the list
+    ListNode prev = null;
+    while (toBeRev != null) {
+      ListNode temp = toBeRev.next;
+      toBeRev.next = prev;
+      prev = toBeRev;
+      toBeRev = temp;
+    }
     
+    // Begin Interleaving
+    ListNode revListHead = prev;
+    
+
+    return null;
   }
-  
+
   private void printList(ListNode head) {
-    while(head != null) {
+    while (head != null) {
       System.out.print(head.val + " -> ");
       head = head.next;
     }
