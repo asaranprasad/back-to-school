d65450500f740a6d83331662ba1b36ce5d339f17
asaranprasad <ambikapathy.s@husky.neu.edu>
2018-12-22 04:11:56 +0530

    practise

--- src/leetcode/CopyPostingsList.java
+++ src/leetcode/CopyPostingsList.java
@@ -1,23 +1,50 @@
 package leetcode;
 
-class PostingsList{
-  int val;
+class PostingsList {
+  char val;
   PostingsList jump;
   PostingsList next;
-  PostingsList(int val){ this.val = val; }
+
+  PostingsList(char val) {
+    this.val = val;
+  }
 }
+
 
 public class CopyPostingsList {
 
   public static void main(String[] args) {
-    PostingsList a = new PostingsList(1);
-    PostingsList b = new PostingsList(2);
-    PostingsList c = new PostingsList(3);
-    PostingsList d = new PostingsList(4);
-    a.next = b; b.next = c; c.next = d; d.next = null;
-    a.jump = c; c.next = b; b.next = d; d.next = d;
+    PostingsList a = new PostingsList('a');
+    PostingsList b = new PostingsList('b');
+    PostingsList c = new PostingsList('c');
+    PostingsList d = new PostingsList('d');
+    a.next = b;
+    b.next = c;
+    c.next = d;
+    d.next = null;
+    a.jump = c;
+    c.next = b;
+    b.next = d;
+    d.next = d;
+
+    CopyPostingsList cpl = new CopyPostingsList();
+    PostingsList copy = cpl.copyPostingsList(a);
+    cpl.printListAndPointers(a);
+    cpl.printListAndPointers(copy);
+  }
+
+  private void printListAndPointers(PostingsList copy) {
+    while (copy != null) {
+      System.out.print(copy.val + " jump: " + copy.jump.val + " -> ");
+    }
+    System.out.println("null");
+  }
+
+  private PostingsList copyPostingsList(PostingsList head) {
+    // create a fresh list with only next ptrs
+        
     
-    
+    return null;
   }
 
 }
