65ac9b0a0e58d72dabb6380a3c2ba9fecd35ae0b
hariprasath <hariprasath.sivanandam@gmail.com>
2018-12-21 07:19:29 +0530

    practise

--- src/leetcode/ZipList.java
+++ src/leetcode/ZipList.java
@@ -1,5 +1,37 @@
 package leetcode;
 
-public class ZipList {
 
+class ListNode{
+  int val;
+  ListNode next;
+  ListNode(int val){ this.val = val; }
+}
+
+public class ZipList {
+  public static void main(String args[]) {
+    ListNode head = new ListNode(0);
+    ListNode node = head;
+    
+    for(int i = 1; i < 7; i++) {
+      node.next = new ListNode(i);
+      node = node.next;
+    }
+    
+    ZipList z = new ZipList();
+    ListNode zipped = z.zipList(head);
+    
+    z.printList(zipped);
+  }
+  
+  private ListNode zipList(ListNode head) {
+    
+  }
+  
+  private void printList(ListNode head) {
+    while(head != null) {
+      System.out.print(head.val + " -> ");
+      head = head.next;
+    }
+    System.out.println("null");
+  }
 }
