8369038847efd71b1337eb1bc4c2c10d5fbbd88f
asaranprasad <ambikapathy.s@husky.neu.edu>
2019-01-24 22:56:07 -0500

    practise

--- src/leetcode/BFSAscendingOrder.java
+++ src/leetcode/BFSAscendingOrder.java
@@ -1,5 +1,5 @@
 package leetcode;
- 
+
 import java.util.*;
 
 public class BFSAscendingOrder {
@@ -12,32 +12,31 @@ public class BFSAscendingOrder {
   
   private List<Integer> bfsAsc(int n, List<Integer> from, List<Integer> to, int host){
     host--;
-    List<Integer> out = new LinkedList<>();
+    List<Integer> out = new List<>();
 
-    List<Integer>[] graph = new List[n];
+    List<Integer>[] graph = new List<>[n];
     for(int i = 0; i < graph.length; i++) graph[i] = new LinkedList<Integer>();
     
-    for(int i = 0; i < from.size(); i++){
-        graph[from.get(i) - 1].add(to.get(i) - 1);
-        graph[to.get(i) - 1].add(from.get(i) - 1);
+    for(int i = 0; i < graph.length; i++){
+    	graph[from - 1].add(to - 1);
+    	graph[to - 1].add(from - 1);
     }
     
     for(int i = 0; i < graph.length; i++) Collections.sort(graph[i]);
 
     Queue<Integer> q = new LinkedList<>();
-    q.add(host);
-    boolean[] visited = new boolean[n];
+	boolean[] visited = new boolean[n];
 
-    while(!q.isEmpty()){
-        int v = q.poll();
-        if(v != host) out.add(v + 1);
-        visited[v] = true;
+	while(!q.isEmpty()){
+		int v = q.poll();
+		if(v != host) out.add(v + 1);
+		visited[v] = true;
 
-        for(int neigh : graph[v]){
-            if(!visited[neigh]) q.add(neigh);
-        }
-    }
-    return out;
+		for(int neigh : graph[v]){
+			if(!visited[neigh]) q.add(neigh);
+		}
+	}
+	return out;
   }
 
 }
---
+++ src/leetcode/CopyListWithRandomPointers.java
@@ -0,0 +1,41 @@
+/**
+ * Definition for singly-linked list with a random pointer.
+ * class RandomListNode {
+ *     int label;
+ *     RandomListNode next, random;
+ *     RandomListNode(int x) { this.label = x; }
+ * };
+ */
+public class Solution {
+    public RandomListNode copyRandomList(RandomListNode head) {
+        if(head == null) return head;
+        
+        // Copy Individual nodes as linked chains
+        RandomListNode node = head;
+        while(node != null){
+            RandomListNode nodeb = new RandomListNode(node.label);
+            nodeb.next = node.next;
+            node.next = nodeb;
+            node = nodeb.next;
+        }
+        
+        // Copy Random Pointers for new node set
+        node = head;
+        while(node != null){
+            if(node.random != null) node.next.random = node.random.next;
+            node = node.next.next;
+        }        
+        
+        // Restore next pointers
+        RandomListNode newHead = head.next;
+        node = head;
+        while(node != null){
+            RandomListNode nodeNext = node.next.next;
+            if(nodeNext != null) node.next.next = nodeNext.next;
+            node.next = nodeNext;
+            node = node.next;
+        }         
+        
+        return newHead;
+    }
+}
