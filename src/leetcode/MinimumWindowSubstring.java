b7d53e71c0f11b838a0f9581240f1435b09323f6
asaranprasad <ambikapathy.s@husky.neu.edu>
2019-01-28 23:57:18 -0500

    practise

--- src/leetcode/MinimumWindowSubstring.java
+++ src/leetcode/MinimumWindowSubstring.java
@@ -4,6 +4,29 @@ import java.util.*;
 
 public class MinimumWindowSubstring {
 
-  
+    public String minWindow(String s, String t) {
+        int[] map = new int[256];
+        int rem = 0;
+        
+        for(char c : t.toCharArray()){
+            map[c]++;
+            rem++;
+        }
+        
+        int min = Integer.MAX_VALUE;
+        
+        int st = 0;
+        int en = 0;
+        
+        while(st <= en){
+            if(rem == 0){
+                if (map[en] > 0) map[en]--;
+                
+            }else{
+                
+            }
+        }
+        
+    }
 
 }
