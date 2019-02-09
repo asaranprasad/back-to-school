af5cd9f159cf3725ba9e6ff91e843207aeecc037
asaranprasad <ambikapathy.s@husky.neu.edu>
2019-02-09 13:05:28 -0500

    practise

--- src/leetcode/KEmptySlots.java
+++ src/leetcode/KEmptySlots.java
@@ -1,42 +1,37 @@
-package leetcode;
-
-public class KEmptySlots {
-
-  public int kEmptySlots(int[] flowers, int k) {
-    int n = flowers.length;
-    boolean[] bloom = new boolean[n];
-    for (int i = 0; i < n; i++) {
-      bloom[flowers[i] - 1] = true;
-      if (kSlotsExists(bloom, k)) {
-        return i + 1;
-      }
+class Solution {
+    public int kEmptySlots(int[] flowers, int k) {
+        int n = flowers.length;
+        
+        boolean[] pots = new boolean[n];
+        
+        for(int i = 0; i < n; i++){
+            int day = i + 1;
+            int pot = flowers[i] - 1;
+            pots[pot] = true;
+                        
+            /* Scan Left */
+            boolean isUnbloomed = true;
+            for(int j = 1; j <= k; j++){
+                if(pot - j >= 0){
+                    if(pots[pot - j]){ isUnbloomed = false; break; }
+                }
+            }
+            if(isUnbloomed){
+                if((pot - (k + 1) >= 0) && pots[pot - (k + 1)]) return day;
+            }   
+            
+            /* Scan Right */
+            isUnbloomed = true;
+            for(int j = 1; j <= k; j++){
+                if(pot + j < n){
+                    if(pots[pot + j]){ isUnbloomed = false; break; }
+                }
+            }
+            if(isUnbloomed){
+                if((pot + (k + 1) < n) && pots[pot + (k + 1)]) return day;
+            }    
+        }
+        
+        return -1;
     }
-    return -1;
-  }
-
-  private boolean kSlotsExists(boolean[] bloom, int k) {
-    int c = 0;
-
-    // search for first bloom
-    int i = 0;
-    while (i < bloom.length && !bloom[i])
-      i++;
-
-    for (; i < bloom.length; i++) {
-      if (bloom[i]) {
-        if (c == k)
-          return true;
-        c = 0;
-      } else
-        c++;
-    }
-    return false;
-  }
-
-  public static void main(String[] args) {
-    KEmptySlots kem = new KEmptySlots();
-    System.out.println(kem.kEmptySlots(new int[] {1, 3, 2}, 1));
-    //System.out.println(kem.kEmptySlots());
-  }
-
 }
